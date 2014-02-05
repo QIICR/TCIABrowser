85297ddcb7830a3092c1c6674d40d3009eff9594
Andrey Fedorov <fedorov@bwh.harvard.edu>
2014-02-05 15:51:31 -0500

    ENH: include additional attributes to be reported

--- TCIABrowser/Util/reportStats.py
+++ TCIABrowser/Util/reportStats.py
@@ -2,6 +2,10 @@ from TCIAClient import *
 
 def getTCIASummary(fileName):
 
+  # which attributes to keep track of
+  seriesAttributes = ['SeriesInstanceUID','Modality','ProtocolName','SeriesDate','BodyPartExamined',\
+      'AnnotationsFlag','Manufacturer','ImageCount']
+
   summaryFile = open(fileName,'w')
 
   tcia_client = TCIAClient()
@@ -13,7 +17,9 @@ def getTCIASummary(fileName):
   import datetime
   startTime = datetime.datetime.now()
 
-  print 'Collection,PatientID,StudyID,Modality,ImageCount,Manufacturer,BodyPartExamined,AnnotationsFlag'
+  for attr in seriesAttributes:
+    summaryFile.write(attr+',')
+  summaryFile.write('\n')
 
   for c in collections:
     response = tcia_client.get_patient(collection=c)
@@ -34,40 +40,14 @@ def getTCIASummary(fileName):
         seriesStr = response.read()[:]
         series = json.loads(seriesStr)
         print '   Study ',s,' (total of ',len(series),' series)'
-        # TODO: collect a spreadsheet with the stats on what is/pop available
-        # in TCIA, make plots with R by modality, number of series, whatever
-        # parameters of the series are available
 
         for ser in series:
-          #summaryFile.write(c+','+p+','+s+','+ser['Modality']+','+int(ser['ImageCount'])+','+ser['Manufacturer']+','+int(ser['AnnotationsFlag'])+'\n')
-          #print c,',',p,',',s,',',ser['Modality'],',',str(ser['ImageCount']),',',ser['Manufacturer']
-          summaryFile.write(c+',')
-          summaryFile.write(p+',')
-          summaryFile.write(s+',')
-          try:
-            summaryFile.write(ser['Modality']+',')
-          except:
-            summaryFile.write('NA,')
-          try:
-            summaryFile.write(str(ser['ImageCount'])+',')
-          except:
-            summaryFile.write('NA,')
-          try:
-            summaryFile.write(ser['Manufacturer']+',')
-          except:
-            summaryFile.write('NA,')
-          try:
-            summaryFile.write(ser['BodyPartExamined']+',')
-          except:
-            summaryFile.write('NA,')
-          try:
-            summaryFile.write(ser['AnnotationsFlag']+'\n')
-          except:
-            summaryFile.write('NA\n')
-
-        # generate 2 plots:
-        # 1) summary of collections: # patients/series
-        # 2) summary by modality: #patients/series per modality
+          for attr in seriesAttributes:
+            try:
+              summaryFile.write(str(ser[attr])+',')
+            except:
+              summaryFile.write('NA,')
+          summaryFile.write('\n')
 
     endTime = datetime.datetime.now()
 
