109f1bf0539b1045f26c1d2b2c020a2c992afdcb
Justin Kirby <2379527+kirbyju@users.noreply.github.com>
2024-10-01 20:21:15 -0400

    Revert "Update TCIAClient.py"
    This reverts commit 5e7acb7a313ea6e082380575505fdcfce2a5ca86.

--- TCIABrowser/TCIABrowserLib/TCIAClient.py
+++ TCIABrowser/TCIABrowserLib/TCIAClient.py
@@ -1,9 +1,9 @@
 import slicer, json, string, urllib.request, urllib.parse, urllib.error
     
 try:
-    slicer.util.pip_install('tcia_utils<1.7.1 -U -q')
+    slicer.util.pip_install('tcia_utils -U -q')
 except:
-    slicer.util.pip_install('tcia_utils<1.7.1')
+    slicer.util.pip_install('tcia_utils')
 import tcia_utils.nbia
 
 #import TCIABrowserLib
